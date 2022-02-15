373593174dce6759050f49d2ccfa8e1ab05379cc
Jayeshjay17 <jayeshramdass17@gmail.com>
2022-02-15 19:04:12 +0530

    some added

---
+++ src/jGit/Sample.java
@@ -0,0 +1,26 @@
+package jGit;
+
+public class Sample {
+
+	private void name() {
+System.out.println("Jayesh");
+	}
+
+	private void id() {
+		System.out.println("1707");
+	}
+
+	private void dob() {
+		System.out.println("17.07.2000");
+	}
+
+	public static void main(String[] args) {
+
+		Sample s = new Sample();
+		s.id();
+		s.name();
+		s.dob();
+
+	}
+
+}
